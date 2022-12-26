d5735f85dba640e425b0f497212b3c87f301d9b9
VitaliySaburdo <vsaburdo@gmail.com>
2022-12-26 20:03:42 +0200

    1

--- src/js/library-storage.js
+++ src/js/library-storage.js
@@ -11,12 +11,22 @@ const libraryEl = document.querySelector('.library-storage-list');
 // }
 
 export function libraryStorage(movieData) {
-  // const filmObject = JSON.stringify(movieData);
+  const filmObject = JSON.stringify(movieData);
   const cartItem = document.querySelector(`[data-id="${movieData.id}"]`);
 
   // Подключаем кнопки модального окна
   const watchBtn = document.querySelector('.js-watched');
   const queueBtn = document.querySelector('.js-queue');
+
+  if (localStorage.getItem('watch').includes(filmObject)) {
+    watchBtn.classList.add('button--accent-btn');
+    watchBtn.textContent = 'REMOVE FROM WATCHED';
+  }
+
+  // if (localStorage.getItem('queue').includes(filmObject)) {
+  //   queueBtn.classList.add('button--accent-btn');
+  //   queueBtn.textContent = 'REMOVE FROM QUEUE';
+  // }
 
   watchBtn.addEventListener('click', () => {
     if (movieData) {
@@ -26,7 +36,7 @@ export function libraryStorage(movieData) {
         watchBtn.textContent = 'ADD TO WATCHED';
         film = film.filter(e => e.id !== movieData.id);
         if (cartItem) {
-          cartItem.remove();
+          localStorage.removeItem('watch');
         }
       } else {
         watchBtn.classList.add('button--accent-btn');
