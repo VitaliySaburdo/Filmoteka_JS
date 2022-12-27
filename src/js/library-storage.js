d1c1b21ff8f7595a6c522f5aa1be93f884f59e22
VitaliySaburdo <vsaburdo@gmail.com>
2022-12-27 16:00:10 +0200

    1

--- src/js/library-storage.js
+++ src/js/library-storage.js
@@ -9,19 +9,25 @@ let queueMovie = [];
 
 export function libraryStorage(movieData) {
   const cartItem = document.querySelector(`[data-id="${movieData.id}"]`);
+  const filmObject = JSON.stringify(movieData);
 
   const watchBtn = document.querySelector('.js-watched');
   const queueBtn = document.querySelector('.js-queue');
+  console.log(localStorage.getItem('watch'));
+  console.log(filmObject);
+  // console.log(getFromStorage('watch').includes(movieData));
 
-  if (getFromStorage('watch')) {
-    watchMovie = getFromStorage('watch');
+  if (
+    localStorage.getItem('watch') !== null &&
+    localStorage.getItem('watch').includes(filmObject)
+  ) {
     watchBtn.textContent = 'REMOVE FROM WATCHED';
   }
 
   watchBtn.addEventListener('click', onWatchBtn);
 
   function onWatchBtn() {
-        if (movieData) {
+    if (movieData) {
       if (watchMovie.find(e => e.id === movieData.id)) {
         watchBtn.classList.remove('button--accent-btn');
         watchBtn.textContent = 'ADD TO WATCHED';
@@ -36,29 +42,30 @@ export function libraryStorage(movieData) {
       }
       addToStorage('watch', watchMovie);
     }
-}
+  }
 
-function renderGalleryFilms(data) {
-  const markup = data
-    .map(
-      ({
-        id,
-        original_title,
-        poster_path,
-        title,
-        genre_ids,
-        release_date,
-      }) => `<li class="gallery__item" data-id="${id}">
+  function renderGalleryFilms(data) {
+    const markup = data
+      .map(
+        ({
+          id,
+          original_title,
+          poster_path,
+          title,
+          genre_ids,
+          release_date,
+        }) => `<li class="gallery__item" data-id="${id}">
         <img src="${renderImg(
           poster_path
         )}" alt="${title}" class="gallery_img" width="395" height="574" />
         <h2 class="gallery__title">${checkTitle(original_title)}</h2>
         <p class="gallery__txt">${changeGenre(genre_ids)} | ${checkDate(
-        release_date
-      )}
+          release_date
+        )}
       </li>`
-    )
-    .join('');
+      )
+      .join('');
 
-  libraryEl.innerHTML = markup;
+    libraryEl.innerHTML = markup;
+  }
 }
