2c45c389d2a5debc6399129c15c9d940074fdbd2
VitaliySaburdo <vsaburdo@gmail.com>
2022-12-27 20:37:47 +0200

    12

--- src/js/library-storage.js
+++ src/js/library-storage.js
@@ -4,6 +4,8 @@ import {
   removeFromStorage,
 } from './localStorage';
 
+export const libraryEl = document.querySelector('.library__list');
+
 let watchMovie = [];
 let queueMovie = [];
 
@@ -46,6 +48,7 @@ export function libraryStorage(movieData) {
       }
       addToStorage('watch', watchMovie);
     }
+    renderGalleryFilms(watchMovie);
   }
 
   function onQueueBtn() {
@@ -65,28 +68,36 @@ export function libraryStorage(movieData) {
       addToStorage('queue', queueMovie);
     }
   }
+
   function renderGalleryFilms(data) {
+    console.log(data);
     const markup = data
       .map(
         ({
           id,
-          original_title,
           poster_path,
           title,
-          genre_ids,
+          genres,
           release_date,
         }) => `<li class="gallery__item" data-id="${id}">
         <img src="${renderImg(
           poster_path
         )}" alt="${title}" class="gallery_img" width="395" height="574" />
-        <h2 class="gallery__title">${checkTitle(original_title)}</h2>
-        <p class="gallery__txt">${changeGenre(genre_ids)} | ${checkDate(
-          release_date
-        )}
+        <h2 class="gallery__title">${title}</h2>
+        <p class="gallery__txt">${genres} | ${release_date.slice(0, 4)}
       </li>`
       )
       .join('');
 
-    libraryEl.innerHTML = markup;
+    if (libraryEl) {
+      libraryEl.innerHTML = markup;
+    }
   }
+}
+
+function renderImg(poster_path) {
+  if (poster_path) {
+    return `https://image.tmdb.org/t/p/w500${poster_path}`;
+  }
+  return `https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png?20170513175923`;
 }
