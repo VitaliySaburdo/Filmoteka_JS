f7467a32a8e8baa87b7871232ea13a58e023f26b
VitaliySaburdo <vsaburdo@gmail.com>
2023-02-04 13:19:58 +0200

    123

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -44,13 +44,9 @@ export default class NewsApiService {
 
   // Запрос по поиску
   async getFilmOnSearch(query, page) {
-    try {
-      const url = `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}`;
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}`;
+    const response = await axios.get(url);
+    return response.data;
   }
   // Запрос информации по жанрам
   async getGenres() {
--- src/js/search.js
+++ src/js/search.js
@@ -6,15 +6,26 @@ const searchForm = document.querySelector('#header-form');
 const cardsContainer = document.querySelector('.gallery-list');
 
 searchForm.addEventListener('submit', onSearch);
+let Page = 1;
+let amountOfPages = 1;
+let query = '';
+let dataArray = [];
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
   query = e.currentTarget.elements.searchQuery.value.trim();
-  console.log(query);
-  NewApiService.getFilmOnSearch(query)
-    .then(data => {
-      cardsContainer.innerHTML = '';
-      renderGalleryFilms(data.results);
-    })
-    .catch(err => console.log(err));
+
+  try {
+    const { results, page, total_pages } = await NewApiService.getFilmOnSearch(
+      query
+    );
+    Page = page;
+    amountOfPages = total_pages;
+    dataArray = results;
+  } catch (error) {
+    console.log(error);
+  }
+  cardsContainer.innerHTML = '';
+  renderGalleryFilms(dataArray);
+  console.log(amountOfPages);
 }
