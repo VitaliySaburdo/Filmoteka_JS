2e338a22b2580002d1cb6d3619dd938f99b4bf61
VitaliySaburdo <vsaburdo@gmail.com>
2023-03-09 06:45:31 +0200

    2

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,3 +9,12 @@ const paginationBar = document.querySelector('.pagination-btns');
 const prevBtn = document.querySelector('.page-btn.prev');
 const nextBtn = document.querySelector('.page-btn.next');
 
+console.log(paginationBar);
+
+async function main() {
+  const data = await ApiService.fetchTrendingMovie();
+  console.log(data);
+  function displayList() {}
+  function displayPagination() {}
+  function displayPaginationBtn() {}
+}
